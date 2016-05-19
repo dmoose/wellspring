15269c128b246227034475ef807f8432cbc0b16c
Jeff Shumate <jeff@catapulsion.net>
2016-05-18 21:20:15 -0600

    cleanup

--- db/migrate/20150831131759_create_wellspring_entries.rb
+++ db/migrate/20150831131759_create_wellspring_entries.rb
@@ -4,12 +4,21 @@ class CreateWellspringEntries < ActiveRecord::Migration
       t.string :type, index: true
       t.string :title
       t.string :slug, index: true
-      t.json :payload
+      t.string :token, index: true
+      t.text   :body
+      t.text   :cached_html
+      t.json   :payload
       t.integer :user_id, index: true
       t.string :author_name
+      t.string :meta_description
       t.datetime :published_at
+      t.integer  :status, default: 0, index: true
+      t.text     :tags, array: true, default: []
+      t.integer  :parent_id, index:true
 
       t.timestamps null: false
     end
+
+    add_index :wellspring_entries, :tags, using: 'gin'
   end
 end
